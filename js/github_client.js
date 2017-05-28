19586ef5ad40f5ed10dc9c93a0971854c8679cc7
gabystern <stern.gabrielle@gmail.com>
2017-05-27 20:17:00 -0400

    in progress

--- js/github_client.js
+++ js/github_client.js
@@ -4,7 +4,14 @@ var createGist = function(file_name, content, description, token){
 };
 
 var myGists = function (username, token){
-
+  $.ajax({
+    url:
+    contentType: 'application/json'
+    dataType: 'jsonp'
+    success: function(){
+      
+    }
+  })
 };
 
 var bindCreateButton = function() {
